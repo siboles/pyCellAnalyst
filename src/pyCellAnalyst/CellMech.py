b7f5dac4e136f937cbf7a975c41a38ad6cea7f9e
siboles <scott.sibole@gmail.com>
2014-03-18 09:37:34 -0600

    Added cell strain calculations and modifed members to reflect homogeneous strain on cells and ECM.

--- src/pyCellAnalyst/CellMech.py
+++ src/pyCellAnalyst/CellMech.py
@@ -12,11 +12,12 @@ class CellMech(object):
            self.rmeshes - list of delaunay tessalations of the reference STL files
            self.dmeshes - list of delaunay tessalations of the deformed STL files
            self.defmeshes - list of the reference delaunay tesselations deformed to the shape of the deformed STLs
-           self.strains - list of numpy arrays containing the strain tensor for each element in the meshes
+           self.cell_strains - list of numpy arrays containing the homogeneous strain tensor for each cell
            self.rvols - volumes of reference state STLs
            self.dvols - volumes of deformed state STLs
-           self.vstrains - volumetric strains of full cells
-           self.effstrains - volume averaged effective strain of full cells
+           self.vstrains - volumetric strains of cells
+           self.effstrains - effective strain of cells
+           self.ecm_strain - ecm strain assuming homogeneity across the region containing cells
            self.rcentroids - centroids of reference state cells
            self.dcentroids - centroids of deformed state cells
     '''
@@ -29,9 +30,12 @@ class CellMech(object):
         self._def_dir = def_dir
         self.rmeshes = []
         self.dmeshes = []
+        self.rsurfs = []
+        self.dsurfs = []
         self.rcentroids = []
         self.dcentroids = []
-        self.strains = []
+        self.cell_strains = []
+        self.ecm_strain = None
         self.rvols = []
         self.dvols = []
         self.vstrains = []
@@ -50,6 +54,7 @@ class CellMech(object):
                 triangles = vtk.vtkTriangleFilter()
                 triangles.SetInputConnection(reader.GetOutputPort())
                 triangles.Update()
+                self.rsurfs.append(triangles)
                 dl = vtk.vtkDelaunay3D()
                 dl.SetInputConnection(triangles.GetOutputPort())
                 dl.Update()
@@ -66,6 +71,7 @@ class CellMech(object):
                 triangles = vtk.vtkTriangleFilter()
                 triangles.SetInputConnection(reader.GetOutputPort())
                 triangles.Update()
+                self.dsurfs.append(triangles)
                 dl = vtk.vtkDelaunay3D()
                 dl.SetInputConnection(triangles.GetOutputPort())
                 dl.Update()
@@ -80,11 +86,10 @@ class CellMech(object):
         for i in xrange(len(self.rcentroids)):
             #create a polydata set of the cell in deformed state
             pd = vtk.vtkGeometryFilter()
-            pd.SetInputConnection(self.dmeshes[i].GetOutputPort())
+            pd.SetInputConnection(self.dsurfs[i].GetOutputPort())
             pd.Update()
             #Now create a modified BSP tree for fast searching
             #http://www.vtk.org/Wiki/VTK/Examples/Cxx/DataStructures/ModifiedBSPTreeIntersectWithLine
-
             tree = vtk.vtkModifiedBSPTree()
             tree.SetDataSet(pd.GetOutput())
             tree.BuildLocator()
@@ -92,49 +97,36 @@ class CellMech(object):
             #find the translation vector to align centroids
             tv = self.dcentroids[i]-self.rcentroids[i]
             #loop over the tetrahedrons and update their vertices to the new position
-            m = self.rmeshes[i]
-            N = m.GetOutput().GetNumberOfCells()
-            local_strains = np.zeros((N,6),float)
+            m = vtk.vtkGeometryFilter()
+            m.SetInputConnection(self.rsurfs[i].GetOutputPort())
+            m.Update()
+            N = m.GetOutput().GetNumberOfPoints()
+            X = np.zeros((N,3),float)
+            x = np.zeros((N,3),float)
             for j in xrange(N):
-                tetra = m.GetOutput().GetCell(j)
-                p0 = [tetra.GetPoints().GetData().GetTuple(0),
-                        tetra.GetPoints().GetData().GetTuple(1),
-                        tetra.GetPoints().GetData().GetTuple(2),
-                        tetra.GetPoints().GetData().GetTuple(3)]
-                for k in xrange(4):
-                    tetra.GetPoints().SetPoint(k,p0[k]+tv)
-                p_def = np.zeros((4,3),float)
-                p_ref = np.zeros((4,3),float)
-                for k in xrange(4):
-                    p1 = np.array(list(tetra.GetPoints().GetData().GetTuple(k)),float)
-                    uv = p1-np.array(p2)
-                    uv /= np.linalg.norm(uv)
-                    pp1 = p1+uv*100
-                    tol = 1e-8
-                    points = vtk.vtkPoints()
-                    cells = vtk.vtkIdList()
-                    tree.IntersectWithLine(pp1,p2,tol,points,cells)
-                    intersection = [0.,0.,0.]
-                    points.GetPoint(0,intersection)
-                    u = np.array(intersection)-p1
-                    p_ref[k,:] = p0[k]+tv
-                    p_def[k,:] = p0[k]+tv+u
-                local_strains[j,:] = self._getTetStrain(p_ref,p_def)
-        print local_strains
+                p = np.array(m.GetOutput().GetPoint(j),float)
+                p1 = p+tv
+                X[j,:] = p1-p2 
+                u = p1-p2
+                u/= np.linalg.norm(u)
+                l = p2+1000*u
+                tol = 1e-8
+                points = vtk.vtkPoints()
+                cells = vtk.vtkIdList()
+                tree.IntersectWithLine(p2,l,tol,points,cells)
+                intersection = [0.,0.,0.]
+                points.GetPoint(0,intersection)
+                x[j,:] = np.array(intersection)-p2
+            self._getCellStrain(X,x)
+            # volumetric strains
+            self.vstrains.append(self.dvols[i]/self.rvols[i]-1)
 
-    def _getTetStrain(self,r,d):
-        X = []
-        x = []
-        for i in xrange(3):
-            for j in xrange(3-i):
-                X.append(r[i,:]-r[j+i+1,:])
-                x.append(d[i,:]-d[j+i+1,:])
-        X = np.array(X,float)
-        x = np.array(x,float)
+    def _getCellStrain(self,X,x):
+        N = X.shape[0]
         #assemble the system
-        dX = np.zeros((6,6),float)
-        ds = np.zeros((6,1),float)
-        for i in xrange(6):
+        dX = np.zeros((N,6),float)
+        ds = np.zeros((N,1),float)
+        for i in xrange(N):
             dX[i,0] = 2*X[i,0]**2
             dX[i,1] = 2*X[i,1]**2
             dX[i,2] = 2*X[i,2]**2
@@ -144,10 +136,9 @@ class CellMech(object):
 
             ds[i,0] = np.linalg.norm(x[i,:])**2-np.linalg.norm(X[i,:])**2
 
-        E = np.linalg.solve(dX,ds)
-        return E.T
-        
-        
+        E = np.linalg.lstsq(dX,ds)[0]
+        E = np.array([[E[0,0],E[3,0],E[4,0]],[E[3,0],E[1,0],E[5,0]],[E[4,0],E[5,0],E[3,0]]],float)
+        self.cell_strains.append(E)
         
 
     def _getmech(self):
@@ -179,8 +170,8 @@ class CellMech(object):
             ds[i,0] = np.linalg.norm(x[i,:])**2-np.linalg.norm(X[i,:])**2
 
         E = np.linalg.lstsq(dX,ds)[0]
-        print E
-            
+        E = np.array([[E[0,0],E[3,0],E[4,0]],[E[3,0],E[1,0],E[5,0]],[E[4,0],E[5,0],E[3,0]]],float)
+        self.ecm_strain = E
 
     def _getMassProps(self,mesh):
         tvol = []
@@ -207,3 +198,4 @@ class CellMech(object):
         cz = np.sum(tvol*tcent[:,2])/volume
         centroid = np.hstack((cx,cy,cz))
         return volume,centroid
+
