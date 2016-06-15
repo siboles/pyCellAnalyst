9215f703f3230396835ebd3eece2d8fe0013300b
Scott Sibole <scott.sibole@gmail.com>
2016-06-15 10:38:56 -0600

    Removed long description from conda setup since README was not being found.

--- src/conda_setup.py
+++ src/conda_setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 from setuptools.dist import Distribution
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(os.path.join(here, os.path.pardir), "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
 
 class BinaryDistribution(Distribution):
     def is_pure(self):
@@ -15,7 +10,6 @@ setup(
     version = '1.0.2',
     description = 'An extensive module for image processing, segmentation, and deformation analysis. Initially aimed at processing 3-D microscopy of cells, this may have applications for other data types as well.',
     packages = ['pyCellAnalyst'],
-    long_description = long_description,
     url = "https://github.com/siboles/pyCellAnalyst",
     author = 'Scott Sibole',
     author_email = 'scott.sibole@gmail.com',
