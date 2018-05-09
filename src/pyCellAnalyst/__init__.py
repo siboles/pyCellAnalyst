2ac3deb9234ad003002e875be69c4e748c667c5c
Scott Sibole <scott.sibole@gmail.com>
2018-05-09 11:08:51 -0600

    Ignore FutureWarning due to vtk numpy_support.

--- src/pyCellAnalyst/__init__.py
+++ src/pyCellAnalyst/__init__.py
@@ -1,6 +1,11 @@
+import warnings
+
+warnings.simplefilter("ignore", category=FutureWarning)
+
 from .Image import *
 from .Filters import *
 from .FilteringPipeline import *
 from .Visualization import *
 from .RegionsOfInterest import *
 from .Segmentation import *
+from .Deformation import *
